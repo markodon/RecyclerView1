6531226671c7cbd881f6dcd417d80276f08326a8
Marko Donovski <markodonovski@live.com>
2017-12-07 18:50:34 +0100

    mainacticity

--- app/src/main/java/com/example/markodonovski/recyclerview1/MainActivity.java
+++ app/src/main/java/com/example/markodonovski/recyclerview1/MainActivity.java
@@ -2,15 +2,36 @@ package com.example.markodonovski.recyclerview1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+
+
+        CustomStudentAdapter adapter = new CustomStudentAdapter();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+
     }
+
+    private void generateList() {
+
+    }
+
+
 }
